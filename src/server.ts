1ad14c8d9fd2f09d4e94741177f7c95970879325
willen <willen.sesantos@gmail.com>
2022-08-20 21:58:54 -0300

    feat:Atualiza server
    adciona comandos para receber informações via json e sincronização com o db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { db } from "./database/db";
+import { router } from "./routes";
 
 const app = express();
 
-app.listen(3000, () => {
+app.use(router);
+app.use(express.json());
+
+app.listen(3000, async () => {
+    await db.sync(); //sincronizando db.
     console.log(`Servidor rodando na porta 3000`);
 })
