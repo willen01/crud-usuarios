e57df438e880d79176a1051e00d0e92ca6ad52de
willen <willen.sesantos@gmail.com>
2022-08-22 22:17:42 -0300

    feat:Cria controller findAll
    Este controller recupera todos os dados do db

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,9 +3,14 @@ import { UserModel } from "../database/models/UserModel";
 
 class UserController {
   async findAll(req: Request, res: Response) {}
-  async findOne(req: Request, res: Response) {}
+  async findOne(req: Request, res: Response) {
+    const users = await UserModel.findAll();
+    return users.length > 0
+      ? res.status(200).json(users)
+      : res.status(204).send();
+  }
   async create(req: Request, res: Response) {
-    const { email, nome, idade } =  req.body;
+    const { email, nome, idade } = req.body;
     const user = await UserModel.create({
       email,
       nome,
